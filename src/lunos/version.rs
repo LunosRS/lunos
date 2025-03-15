e8db802c4c8e4c479589c485dd39d922623e897f
thoq <tristanshaw000@gmail.com>
2025-03-14 22:18:47 -0500

    oopsie poopsie

--- src/lunos/version.rs
+++ src/lunos/version.rs
@@ -4,7 +4,7 @@ pub fn show(exit_code: i32) {
     print!("{}", ASCII_BANNER);
     println!("{} v{}", NAME, VERSION);
     println!("{} by {}", DESCRIPTION, AUTHORS);
-    println!("(LEAGAL) License: {}", LICENSE);
+    println!("(LEGAL) License: {}", LICENSE);
 
     std::process::exit(exit_code);
 }
